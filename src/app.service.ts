a7d7d8c3140427a813f7c979daabfc99237f46a6
Alvaro Segovia <alvarosego01@gmail.com>
2023-11-13 09:15:45 -0400

    solucion test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola luichi te amo...!!';
+    return 'Hello World!';
   }
 }
